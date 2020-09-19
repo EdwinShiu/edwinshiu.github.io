b0fca07b196ee662dd2dc8a4bf84e7b491d37bd9
Edwin Shiu <edwin2000412@gmail.com>
2020-09-19 16:47:52 +0800

    contact

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
-
+// form textarea animation
 $(document).ready(function() {
-  
+  $("#form-textarea").focus(function() {
+    $(".form-textarea-content").css("color", "var(--primary-text-color)");
+  });
+  $("#form-textarea").focusout(function() {
+    if ($("form textarea").val() == "") {
+      $(".form-textarea-content").css("color", "var(--primary-border-color)");
+    }
+  });
 })
 
 
